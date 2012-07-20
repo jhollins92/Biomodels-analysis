af9f32045eb4906f287f318927d6bb7be38ce756
James Hollins <b0033126@starlink.ncl.ac.uk>
2012-07-20 11:52:33 +0100

    Conflicting types for the variable "comparts" fixed. Average number of connections now just shown on console.

--- average_number_of_connections.R
+++ average_number_of_connections.R
@@ -1,7 +1,6 @@
 functionnames = c("AverageNumberOfConnections", "ConvertDateStringsToYears", "CummulativeNumberOfModelsInEachYear", "CummulativeNumberOfSpeciesInEachYear", "DataFrameOfsboTermsinAllModels", "DataFrameOfsboTermsInaModel", "FindAllYears", "FrequencyOfsboTermsInaModel", "GetAllConnections", "GetAllConnectionsInAModel", "GetAllSpeciesFromAYear", "GetModelsboTerms", "GetSpeciesIDsForaModel", "GetStartDateStrings", "GetUniqueSpecies", "IdentifyAllModelsInAnyGivenYear", "OrderSpeciesIDs", "ReactionConnections", "SortOutAllConnections")
 source("functions3.R")
 allconnections = SortOutAllConnections(GetAllConnections(Models))
-averagenumberofconnectionsperspecies = AverageNumberOfConnections(allconnections)
-averagenumberofconnectionsperspecies
+AverageNumberOfConnections(allconnections)
 rm(list=functionnames)
 rm(functionnames)
