6ea40ae2ba65ca2a597085b2c18082ccadd32b6f
James Hollins <b0033126@starlink.ncl.ac.uk>
2012-08-16 18:12:46 +0100

    Colour added to graphs.

--- graphics.R
+++ graphics.R
@@ -2,27 +2,27 @@
 dir.create("Graphs")
 
 pdf("Graphs/CompartmentBarplot.pdf", width=6, height=6)
-barplot(comparts_summary[[1]], names.arg=comparts_summary[[2]], xlab="Number of Compartments", ylab="Frequency", main="Barplot of the Number of Compartments \n in each Biomodel", ylim=c(0,250)) 
+barplot(comparts_summary[[1]], names.arg=comparts_summary[[2]], xlab="Number of Compartments", ylab="Frequency", main="Barplot of the Number of Compartments \n in each Biomodel", ylim=c(0,250), col="lightskyblue") 
 dev.off()
 
 pdf("Graphs/SpeciesHistogram.pdf", width=6, height=6)
-hist(specs[specs < 200], breaks = 40, xlim = c(0,200), ylim = c(0, 120), xlab="Number of Species", ylab="Frequency", main="Histogram of the Number of Species \n in each Biomodel") 
+hist(specs[specs < 200], breaks = 40, xlim = c(0,200), ylim = c(0, 120), xlab="Number of Species", ylab="Frequency", main="Histogram of the Number of Species \n in each Biomodel", col="lightskyblue") 
 dev.off()
 
 pdf("Graphs/ReactionsHistogram.pdf", width=6, height=6)
-hist(reacts[reacts < 200], breaks = 40, xlim=c(0, 200), ylim = c(0, 120), xlab="Number of Reactions", ylab="Frequency", main="Histogram of the Number of Reactions \n in each Biomodel")
+hist(reacts[reacts < 200], breaks = 40, xlim=c(0, 200), ylim = c(0, 120), xlab="Number of Reactions", ylab="Frequency", main="Histogram of the Number of Reactions \n in each Biomodel", col="lightskyblue")
 dev.off()
 
 pdf("Graphs/GlobalParametersHistogram.pdf", width=6, height=6)
-hist(globalparams, breaks = 30, xlim = c(0, 300), ylim = c(0, 200), xlab="Number of Global Parameters", ylab="Frequency", main="Histogram of the Number of Global Parameters \n in each Biomodel")
+hist(globalparams, breaks = 30, xlim = c(0, 300), ylim = c(0, 200), xlab="Number of Global Parameters", ylab="Frequency", main="Histogram of the Number of Global Parameters \n in each Biomodel", col="lightskyblue")
 dev.off()
 
 pdf("Graphs/LocalParametersHistogram.pdf", width=6, height=6)
-hist(localparams[localparams < 400], breaks = 40, xlim=c(0, 400), ylim=c(0, 300), xlab="Number of Local Parameters", ylab="Frequency", main="Histogram of the Number of Local Parameters \n in each Biomodel")
+hist(localparams[localparams < 400], breaks = 40, xlim=c(0, 400), ylim=c(0, 300), xlab="Number of Local Parameters", ylab="Frequency", main="Histogram of the Number of Local Parameters \n in each Biomodel", col="lightskyblue")
 dev.off()
 
 pdf("Graphs/SpeciesOverReactions.pdf", width=6, height=6)
-hist(spectoreactratio, ylim=c(0, 150), xlab="Ratio of Species over Reactions", main="Histogram of the Ratio of Species over Reactions\n for Each Biomodel")
+hist(spectoreactratio, ylim=c(0, 150), xlab="Ratio of Species over Reactions", main="Histogram of the Ratio of Species over Reactions\n for Each Biomodel", col="lightskyblue")
 dev.off()
 ## Number of Models and Species against Time Plots
 
