359164ac376a592b842f49504b262d44dc0b4dfb
James Hollins <b0033126@starlink.ncl.ac.uk>
2012-08-16 16:42:26 +0100

    Protein binding picture added

--- average_number_of_connections.R
+++ average_number_of_connections.R
@@ -1,6 +1,6 @@
 functionnames = c("AverageNumberOfConnections", "ConvertDateStringsToYears", "CummulativeNumberOfModelsInEachYear", "CummulativeNumberOfSpeciesInEachYear", "DataFrameOfsboTermsinAllModels", "DataFrameOfsboTermsInaModel", "FindAllYears", "FrequencyOfsboTermsInaModel", "GetAllConnections", "GetAllConnectionsInAModel", "GetAllSpeciesFromAYear", "GetModelsboTerms", "GetSpeciesIDsForaModel", "GetStartDateStrings", "GetUniqueSpecies", "IdentifyAllModelsInAnyGivenYear", "OrderSpeciesIDs", "ReactionConnections", "SortOutAllConnections")
 source("functions3.R")
 allconnections = SortOutAllConnections(GetAllConnections(Models))
-round(AverageNumberOfConnections(allconnections), digits=4)
+round(AverageNumberOfConnections(allconnections), digits=2)
 rm(list=functionnames)
 rm(functionnames)
